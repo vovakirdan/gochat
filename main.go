a865912f504769c80e20cb6e125712ca56e934e1
Vladimir <vladimir@MacBook-Pro-nikolay.local>
2024-12-13 21:35:19 +0300

    specify ip and port if you want

--- main.go
+++ main.go
@@ -3,19 +3,33 @@ package main
 import (
 	"log"
 	"net"
+	"flag"
+	"fmt"
 )
 
 func main() {
+	defaultIP := "127.0.0.1"
+	defaultPort := "7878"
+
+	// Определение флагов
+	ip := flag.String("ip", defaultIP, "IP address to bind the server to")
+	port := flag.String("port", defaultPort, "Port to bind the server to")
+
+	// Разбор флагов
+	flag.Parse()
+
+	// Формируем адрес из IP и порта
+	address := fmt.Sprintf("%s:%s", *ip, *port)
 	server := NewServer()
 	go server.Run()
 
-	listener, err := net.Listen("tcp", ":7878")
+	listener, err := net.Listen("tcp", address)
 	if err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
 	defer listener.Close()
 
-	log.Println("Server started on :7878")
+	log.Printf("Server started on %s\n", address)
 
 	for {
 		conn, err := listener.Accept()
