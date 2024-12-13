08babe0c693ffe06438e5d839c7c5becf3108a68
vovakirdan <vova.kirdan@mail.ru>
2024-12-13 15:42:59 +0300

    some commands and rooms

--- database.go
+++ database.go
@@ -1,13 +1,17 @@
 package main
 
+// todo hash all passwords
 type Database struct {
 	users map[string]string  // username -> password
+	rooms map[string]string  // rooms -> password; if no password room is open
 }
 
 func NewDatabase() *Database {
-	return &Database{
+	db := &Database{
 		users: make(map[string]string),
+		rooms: make(map[string]string),
 	}
+	return db
 }
 
 func (db *Database) Register(username, password string) bool {
@@ -21,4 +25,18 @@ func (db *Database) Register(username, password string) bool {
 func (db *Database) ValidateUser(username, password string) bool {
 	pass, exists := db.users[username]
 	return exists && pass == password
+}
+
+func (db *Database) AddRoom(room, password string) bool {
+	if _, ok := db.rooms[room]; ok {
+		return false
+	}
+	db.rooms[room] = password
+	return true
+}
+
+func (db *Database) IsPrivateRoom(room string) bool {
+	// if password is empty, room is open
+	room_password, exists := db.rooms[room]
+	return exists && room_password != "" 
 }
--- server.go
+++ server.go
@@ -6,6 +6,7 @@ import (
 	"net"
 	"strings"
 	"sync"
+	"time"
 )
 
 type Server struct {
@@ -59,28 +60,49 @@ func (s *Server) HandleConnection(conn net.Conn) {
 			return
 		}
 		
-		// if command
-		message = strings.TrimSpace(message)
-		if strings.HasPrefix(message, "/room") {
-			// move to the new room example
-			// todo refactor
-			newRoom := strings.TrimPrefix(message, "/room ")
-			s.ChangeRoom(username, newRoom)
-			continue
+		status, err := s.HandleMessage(client, message)
+		if err != nil {
+			fmt.Println("Error handling message:", err)
+			return
 		}
-
-		// if empty message
-		if message == "" {
-			s.EmptyMessage(*client)
-			continue
+		
+		switch status {
+		case 1:
+			s.Info(fmt.Sprintf("User (%s) in room [%s] typed wrong command: %s\n", username, client.Room, message))
+		case 2:
+			fmt.Printf("User %s in room %s typed private message: %s\n", username, client.Room, message)
 		}
-
-		// or else broadcast to room
-		s.BroadcastMessage(*client, message)
 	}
 }
 
-func (s *Server) BroadcastMessage(sender ClientContext, message string) {
+func (s *Server) HandleMessage(client *ClientContext, message string) (int, error) {
+	message = strings.TrimSpace(message)
+	// if command
+	if strings.HasPrefix(message, "/") {
+		if s.ParseCommand(client, message) {
+			// if everything is ok
+			return 0, nil
+		} else {
+			s.SystemMessage(client, "Invalid or unknown command. Type /help <command> to see commands.")
+			return 1, nil
+		}
+	} 
+	// if private message
+	if strings.HasPrefix(message, "@") {
+		// do private message
+		return 0, nil
+	}
+	// if empty message
+	if message == "" {
+		s.EmptyMessage(client)
+		return 0, nil
+	}
+	// or else broadcast to room
+	s.BroadcastMessage(client, message)
+	return 0, nil
+}
+
+func (s *Server) BroadcastMessage(sender *ClientContext, message string) {
 	s.clientsMu.Lock()
 	defer s.clientsMu.Unlock()
 
@@ -88,23 +110,41 @@ func (s *Server) BroadcastMessage(sender ClientContext, message string) {
 		if client.Username != sender.Username && client.Room == sender.Room {
 			// fmt.Fprintf(client.Conn, "[%s]: %s\n", sender, message)
 			s.EmptyMessage(sender)
-			s.SendMessage(*client, sender, message)
+			s.SendMessage(client, sender, message)
 		}
 	}
 }
 
-func (s *Server) ChangeRoom(username, newRoom string) {
+func (s *Server) CreateRoom(client *ClientContext, roomName, password string) bool {
+	if s.database.AddRoom(roomName, password) {
+		s.SystemMessage(client, fmt.Sprintf("Room '%s' created.\n", roomName))
+		return true
+	}
+	s.SystemMessage(client, "Room with this name already exists.")
+	return false
+}
+
+func (s *Server) ChangeRoom(username, newRoom, givenPassword string) bool {
 	s.clientsMu.Lock()
 	defer s.clientsMu.Unlock()
 
 	client, exists := s.clients[username]
 	if !exists {  // strange place
-		return
+		return false
+	}
+	room_password, exists := s.database.rooms[newRoom]
+	if !exists {
+		s.SystemMessage(client, "Room with this name doesn't exist.")
+		return false
+	}
+	if room_password != "" && room_password != givenPassword {
+		s.SystemMessage(client, "Wrong password.")
+		return false
 	}
 
 	client.Room = newRoom
-	fmt.Fprintf(client.Conn, "You have been moved to the '%s' room.\n", newRoom)
-	s.EmptyMessage(*client)
+	s.SystemMessage(client, fmt.Sprintf("You have been moved to the '%s' room.\n", newRoom))
+	return true
 }
 
 func (s *Server) RemoveClient(username string) {
@@ -117,14 +157,123 @@ func (s *Server) RemoveClient(username string) {
 // ------------
 // utils
 
-func (s *Server) SystemMessage(conn net.Conn, message string) {
-	fmt.Fprintf(conn, "(system): %s\n", message)
+func (s *Server) SystemMessage(client *ClientContext, message string) {
+	fmt.Fprintf(client.Conn, "\n(system): %s\n", message)
+	s.EmptyMessage(client)
 }
 
-func (s *Server) SendMessage(client, sender ClientContext, message string) {
+func (s *Server) SendMessage(client, sender *ClientContext, message string) {
 	fmt.Fprintf(client.Conn, "\n[%s] %s: %s\n(%s): ", sender.Room, sender.Username, message, client.Username)
 }
 
-func (s *Server) EmptyMessage(client ClientContext) {
+func (s *Server) EmptyMessage(client *ClientContext) {
 	fmt.Fprintf(client.Conn, "(%s): ", client.Username)
+}
+
+func (s *Server) Info(message string) {
+	fmt.Print("[")
+	fmt.Print(time.Now().Format("2006-01-02 15:04:05"))
+	fmt.Print("] [INFO]")
+	fmt.Println(message)
+}
+
+func (s *Server) ListSomethingToClient(client *ClientContext, what string) {
+	switch what {
+	case "rooms":
+		// iterate over database.rooms and create a message for client
+		s.SystemMessage(client, "Available rooms:")
+		i := 0
+		for roomName := range s.database.rooms {
+			if s.database.IsPrivateRoom(roomName) {
+				fmt.Fprintf(client.Conn, "%d) %s private\n", i, roomName)
+			} else {
+				fmt.Fprintf(client.Conn, "%d) %s\n", i, roomName)
+			}
+			i++
+		}
+	case "users":
+		// iterate over database.users and create a message for client
+		s.SystemMessage(client, "Available users:")
+		i := 0
+		for username := range s.database.users {
+			fmt.Fprintf(client.Conn, "%d) %s\n", i, username)
+			i++
+		}
+	default:
+		s.SystemMessage(client, "Unknown list type")
+	}
+	s.EmptyMessage(client)
+}
+
+func (s *Server) ParseCommand(client *ClientContext, message string) bool {
+	// assume message starts with "/"
+	// unlock client
+	// s.clientsMu.Lock()
+	// defer s.clientsMu.Unlock()
+
+	// preprocess message: trim, split, etc
+	message = strings.TrimSpace(message)
+	// split by parts
+	parts := strings.Split(message, " ")
+	argscount := len(parts)
+	if len(parts) == 0 {
+		// empty command
+		return false
+	}
+
+	command := parts[0]
+	switch command {
+	case "/switch":
+		// move to the new room example
+		if argscount < 2 {
+			s.SystemMessage(client, "You need to specify what do you want to switch.")
+			return true
+		}
+		switch parts[1] {
+		case "room":
+			if argscount < 3 {
+				s.ListSomethingToClient(client, "rooms")
+				return true
+			} 
+			if argscount == 3 {
+				s.ChangeRoom(client.Username, parts[2], "")  // non-private room
+				return true
+			} 
+			if argscount == 4 {
+				s.ChangeRoom(client.Username, parts[2], parts[3])  // private room
+			}
+		default:
+			// unknown switch type
+			return false
+		}
+		return true
+	case "/create":
+		// create room example
+		if argscount < 2 {
+			s.SystemMessage(client, "You need to specify what do you want to create.")
+			return true
+		}
+		switch parts[1] {
+		case "room":
+			if argscount < 3 {
+				s.SystemMessage(client, "You need to specify room name and (if neccessary) password")
+				return true
+			} 
+			if argscount == 3 {
+				s.CreateRoom(client, parts[2], "")  // non-private room
+				return true
+			} 
+			if argscount == 4 {
+				s.CreateRoom(client, parts[2], parts[3])  // private room
+				return true
+			}
+		default:
+			// unknown create type
+			return false
+		}
+		return true
+	default:
+		// unknown command
+		return false
+	}
 }
